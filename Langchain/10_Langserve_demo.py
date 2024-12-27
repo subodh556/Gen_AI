9a184c04ff6d3f0d9bafbb2dd93e8f16dbceee68
Subodh <sohonisubodh@gmail.com>
2024-12-27 22:15:24 +0530

    Langserve

---
+++ Langchain/10_Langserve_demo.py
@@ -0,0 +1,42 @@
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv, find_dotenv
+from fastapi import FastAPI
+from langserve import add_routes
+import uvicorn
+
+
+_ = load_dotenv(find_dotenv())
+
+chatm = ChatGroq(
+    model="llama3-70b-8192"
+)
+
+parser = StrOutputParser()
+
+system_template = "Translate the following into {language}:"
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+chain = prompt_template | chatm | parser
+
+
+app = FastAPI(
+  title="simpleTranslator",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
