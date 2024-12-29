98f0cbf94aa47968d7123fa2592b455ebc92e409
Subodh <sohonisubodh@gmail.com>
2024-12-29 16:16:40 +0530

    level-2 projects 06_Interview_Assistant

---
+++ Level-2_Projects/06_Interview_Assitant/README.md
@@ -0,0 +1,17 @@
+
+
+1. Create an environment
+
+```bash
+conda create -n interview python=3.10 -y
+
+
+conda activate interview
+
+```
+
+2. install requirements
+
+```bash
+pip install -r requirements.txt
+```
---
+++ Level-2_Projects/06_Interview_Assitant/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Subodh",
+    author_email="subodh@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
